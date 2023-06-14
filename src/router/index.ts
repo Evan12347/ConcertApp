08e6926a9abdc518dcdc2a34f23c02bfe2f6540f
Evan12347 <evan90000@hotmail.fr>
2023-06-14 19:33:36 +0200

    V1

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import HomePage from '../views/HomePage.vue'
+import LoginPage from "@/views/LoginPage.vue";
+import RegisterPage from "@/views/RegisterPage.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterPage
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {IonPage, IonContent, IonInput, IonButton, IonImg} from '@ionic/vue';
+
+import {ref} from 'vue';
+
+const username = ref('');
+const password = ref('');
+</script>
+
+<template>
+  <ion-page>
+    <ion-content>
+      <div>
+        <ion-img
+            src="https://img.freepik.com/photos-gratuite/vue-arriere-foule-fans-regardant-performances-direct-concert-musique-nuit-espace-copie_637285-544.jpg?w=2000"
+            style="width: 50%;
+             margin: auto;
+             padding: 1rem">
+        </ion-img>
+        <form>
+          <ion-item>
+            <ion-label position="floating">Nom d'utilisateur</ion-label>
+            <ion-input
+                type="text"
+                fill="outline"
+                v-model="username"
+                required>
+            </ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label position="floating">Mot de passe</ion-label>
+            <ion-input
+                type="password"
+                fill="outline"
+                v-model="password"
+                required>
+            </ion-input>
+          </ion-item>
+          <ion-button type="submit">Connexion</ion-button>
+
+        </form>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
